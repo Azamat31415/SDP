cbb38c0b2a716d0e666536fcf44d771d6f7566a0
Azamat31415 <230147@astanait.edu.kz>
2024-10-01 21:07:53 +0500

    Creation of abstract Taxi class as part of the Factory pattern implementation

--- src/Taxi.java
+++ src/Taxi.java
@@ -1,2 +1,11 @@
-public class Taxi {
+public abstract class Taxi {
+    private String driverName;
+    private String license;
+
+    public Taxi(String driverName, String license) {
+        this.driverName = driverName;
+        this.license = license;
+    }
+
+    public abstract void takePassenger(String passenger);
 }
