0eddb6c492f93364233ab0b9f4bda008adc1e488
Azamat31415 <230147@astanait.edu.kz>
2024-10-01 21:20:40 +0500

    Implemented PremiumTaxiFactory for creating PremiumTaxi instances

--- src/PremiumTaxiFactory.java
+++ src/PremiumTaxiFactory.java
@@ -1,2 +1,6 @@
-public class PremiumTaxiFactory {
+public class PremiumTaxiFactory extends Factory {
+    @Override
+    public Taxi createTaxi(String driverName, String license) {
+        return new PremiumTaxi(driverName, license);
+    }
 }
