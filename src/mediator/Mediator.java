b4978a8f8b7fa2c6b5b806f175e1c49fe0f3d14b
Azamat31415 <230147@astanait.edu.kz>
2024-10-22 21:41:51 +0500

    Added Observer Pattern

--- src/mediator/Mediator.java
+++
@@ -1,5 +0,0 @@
-package mediator;
-
-public interface Mediator {
-    void notify(Object sender, String event);
-}
---
+++ src/observer/Client.java
@@ -0,0 +1,14 @@
+package observer;
+
+public class Client implements Observer {
+    private String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String status) {
+        System.out.println("Client " + name + " received update: " + status);
+    }
+}
